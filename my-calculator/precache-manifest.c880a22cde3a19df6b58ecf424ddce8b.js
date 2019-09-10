da2932302ad0f5e8a1bc4ad3f3c8ec21989a436f
dingqiang <476958714@qq.com>
2019-09-10 17:55:34 +0800

    测试1

---
+++ my-calculator/precache-manifest.c880a22cde3a19df6b58ecf424ddce8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fb0170b308925a6eded594a41997fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ac83beb9834f85c8ce4",
+    "url": "./static/css/main.43999dc4.chunk.css"
+  },
+  {
+    "revision": "aca8fac738d75baca5d0",
+    "url": "./static/js/2.8dd4ee5f.chunk.js"
+  },
+  {
+    "revision": "6ac83beb9834f85c8ce4",
+    "url": "./static/js/main.61bb3b8f.chunk.js"
+  },
+  {
+    "revision": "5f1858af2e083300496c",
+    "url": "./static/js/runtime~main.08f4dc42.js"
+  }
+]);
